e83c3b7f6f1f99e0243d109677d2cff2b2bd93e3
MarkSun <mark@weintek.com>
2020-03-17 11:04:24 +0800

    Modify replace code

--- mytools/search.sh
+++ mytools/search.sh
@@ -1,5 +1,5 @@
 find . -name "*.o" >> buildfile.txt
-sed 's/.o/.c/g' < buildfile.txt > replacefile.txt
+sed 's/\.o/\.c/g' < buildfile.txt > replacefile.txt
 rm buildfile.txt
 
 exec < replacefile.txt
@@ -7,7 +7,7 @@ exec < replacefile.txt
 while read line
 do
 	#echo $line # 一行一行印出內容
-	grep -Hsnr $1 $line
+	grep -Hsnr "$1" $line
 done
 
 rm replacefile.txt
